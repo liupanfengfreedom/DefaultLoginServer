c0e204e6bf1f55e800d62745a757a4942ea8f0c0
liupanfeng <liupanfengfreedom@gmail.com>
2020-07-10 19:10:59 +0800

    anti

--- HttpListenerContextClass.cs
+++ HttpListenerContextClass.cs
@@ -28,63 +28,104 @@ namespace DefaultLoginServer
         {
             Console.WriteLine("HttpListenerContextClass deconstruct");
         }
+        void Posthandle(HttpListenerRequest request)
+        {
+            System.IO.Stream input = request.InputStream;
+            byte[] array = new byte[request.ContentLength64];
+            input.Read(array, 0, (int)request.ContentLength64);//larg file may encounter error
+            string utfString = Encoding.UTF8.GetString(array, 0, array.Length);
+            Console.WriteLine(utfString);
+            HttpListenerResponse response = mhttplistenercontext.Response;
+            // Construct a response.
+            string responseString = "success";
+            //responseString += utfString;
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            Thread.Sleep(90);
+            input.Close();
+            output.Close();
+        }
+        void gethandle(HttpListenerRequest request)
+        {
+            System.Collections.Specialized.NameValueCollection header = request.Headers;
+            string[] headerallkeys = header.AllKeys;
+            string mid = "";
+            string rarpath = "";
+            foreach (var a in headerallkeys)
+            {
+                if (a.Equals("Matchserver"))
+                {
+                    System.IO.Stream input1 = request.InputStream;
+                    byte[] array1 = new byte[request.ContentLength64];
+                    input1.Read(array1, 0, (int)request.ContentLength64);//larg file may encounter error
+                    string utfString1 = Encoding.UTF8.GetString(array1, 0, array1.Length);
+                    Console.WriteLine(utfString1);
+                    Program.roomlist = JsonConvert.DeserializeObject<FRoomlist>(utfString1);
+                    input1.Close();
+
+                    HttpListenerResponse response1 = mhttplistenercontext.Response;
+                    // Construct a response.
+                    byte[] buffer1 = System.Text.Encoding.UTF8.GetBytes("success");
+                    System.IO.Stream output1 = response1.OutputStream;
+                    output1.Write(buffer1, 0, buffer1.Length);
+                    Thread.Sleep(500);
+                    output1.Close();
+                }
+                if (a.Equals("User-Agent"))
+                {
+                    MySQLOperation msqlo = MySQLOperation.getinstance();
+                    string cmd = String.Format(
+                      "SELECT * FROM {0}", "animationtable_user1"
+                    );
+                    List<List<object>> rv = msqlo.get(cmd);
+                    string strPayload = JsonConvert.SerializeObject(rv);
+                    //Console.WriteLine("username");
+                    //string strPayload = JsonConvert.SerializeObject(Program.roomlist);
+                    HttpListenerResponse response1 = mhttplistenercontext.Response;
+                    // Construct a response.
+                    string responseString1 = "success";
+                    //responseString += utfString;
+                    byte[] buffer1 = System.Text.Encoding.UTF8.GetBytes(strPayload);
+                    // Get a response stream and write the response to it.
+                    response1.ContentLength64 = buffer1.Length;
+                    System.IO.Stream output1 = response1.OutputStream;
+                    output1.Write(buffer1, 0, buffer1.Length);
+                    Thread.Sleep(500);
+                    output1.Close();
+                }
+                else if (a.Equals("rarPath"))//
+                {
+                    string[] values = header.GetValues(a);
+                    rarpath = values[0];
+                }
+                else if (a.Equals("pakCallbackUrl"))//
+                {
+                    string[] values = header.GetValues(a);
+                }
+            }
+
+        }
         void handlethreadfunc()
         {
             try
             {
                 HttpListenerRequest request = mhttplistenercontext.Request;
-                Stream stream = request.InputStream;
-                
-                System.Collections.Specialized.NameValueCollection header = request.Headers;
-                string[] headerallkeys = header.AllKeys;
-                string mid = "";
-                string rarpath = "";
-                foreach (var a in headerallkeys)
+                if (request.HttpMethod.Equals("POST"))
                 {
-                    if (a.Equals("Matchserver"))
-                    {
-                        System.IO.Stream input1 = request.InputStream;
-                        byte[] array1 = new byte[request.ContentLength64];
-                        input1.Read(array1, 0, (int)request.ContentLength64);//larg file may encounter error
-                        string utfString1 = Encoding.UTF8.GetString(array1, 0, array1.Length);
-                        Console.WriteLine(utfString1);
-                        Program.roomlist = JsonConvert.DeserializeObject<FRoomlist>(utfString1);
-                        input1.Close();
+                    Posthandle(request);
 
-                        HttpListenerResponse response1 = mhttplistenercontext.Response;
-                        // Construct a response.
-                        byte[] buffer1 = System.Text.Encoding.UTF8.GetBytes("success");
-                        System.IO.Stream output1 = response1.OutputStream;
-                        output1.Write(buffer1, 0, buffer1.Length);
-                        Thread.Sleep(500);
-                        output1.Close();
-                    }
-                    if (a.Equals("UserName"))
-                    {
-                        //Console.WriteLine("username");
-                        string strPayload = JsonConvert.SerializeObject(Program.roomlist);
-                        HttpListenerResponse response1 = mhttplistenercontext.Response;
-                        // Construct a response.
-                        string responseString1 = "success";
-                        //responseString += utfString;
-                        byte[] buffer1 = System.Text.Encoding.UTF8.GetBytes(strPayload);
-                        // Get a response stream and write the response to it.
-                        response1.ContentLength64 = buffer1.Length;
-                        System.IO.Stream output1 = response1.OutputStream;
-                        output1.Write(buffer1, 0, buffer1.Length);
-                        Thread.Sleep(500);
-                        output1.Close();
-                    }
-                    else if (a.Equals("rarPath"))//
-                    {
-                        string[] values = header.GetValues(a);
-                        rarpath = values[0];
-                    }
-                    else if (a.Equals("pakCallbackUrl"))//
-                    {
-                        string[] values = header.GetValues(a);
-                    }
                 }
+                else if (request.HttpMethod.Equals("GET"))
+                {
+                    gethandle( request);
+                }
+             
+               // Stream stream = request.InputStream;
+
+                
 
                 //foreach (var a in headerallkeys)
                 //{
@@ -100,10 +141,8 @@ namespace DefaultLoginServer
                 //    }
                 //}
 
-                //System.IO.Stream input = request.InputStream;
-                //byte[] array = new byte[request.ContentLength64];
-                //input.Read(array, 0, (int)request.ContentLength64);//larg file may encounter error
-                //string utfString = Encoding.UTF8.GetString(array, 0, array.Length);
+
+
 
                 //HttpListenerResponse response = mhttplistenercontext.Response;
                 //// Construct a response.
